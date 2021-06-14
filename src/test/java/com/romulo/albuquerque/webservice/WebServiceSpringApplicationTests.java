a18101d557deb38ccec3370b879d64fa0a135324
Romulo Albuquerque <romulo.albuquerque@live.com>
2021-06-14 13:28:37 -0300

    Created Project

---
+++ src/test/java/com/romulo/albuquerque/webservice/WebServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romulo.albuquerque.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
